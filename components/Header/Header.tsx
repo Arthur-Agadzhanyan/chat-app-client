3a99fdf2a25acf2b5542f7ee27eed03e97dc7379
Arthur-Agadzhanyan <artur0505gold@gmail.com>
2021-07-21 19:41:50 +0300

    added: navigation button on the header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,11 +1,52 @@
-import React from 'react';
-import { AppBar, Toolbar, Typography, Grid } from '@material-ui/core';
+import React, { FC } from 'react';
+import { AppBar, Toolbar, Typography, Grid, Card, CardHeader, Avatar, Box, Popper, Fade, Paper, PopperPlacementType, Button, Menu, MenuItem } from '@material-ui/core';
+import Link from "next/link"
+import { HeaderProps } from './interfaces';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import HeaderStyles from './header.style';
 
-const Header = () => {
+
+const Header: FC<HeaderProps> = ({ store }) => {
     const classes = HeaderStyles()
+
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    }
+
     return (
         <>
+            <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'center',
+                }}
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'center',
+                }}
+                elevation={0}
+                className={classes.menu}
+            >
+                <Paper className={classes.poppup}>
+                    <Typography className={classes.poppup__title}>Артур Агаджанян</Typography>
+                    <Link href="/settings">
+                        <MenuItem onClick={handleClose} className={classes.poppup__link}>Настройки</MenuItem>
+                    </Link>
+                    <MenuItem onClick={handleClose} className={classes.poppup__link}>Выйти</MenuItem>
+                </Paper>
+            </Menu>
+
             <AppBar className={classes.appBar} position="static" elevation={0} color="primary">
                 <Grid className={classes.content} container>
                     <Grid item sm={9} md={8} lg={7} xl={6}>
@@ -16,6 +57,16 @@ const Header = () => {
                             <Typography variant="h6" className={classes.title}>
                                 Типа крутой бренд
                             </Typography>
+
+                            <Box className={classes.avatar} onClick={handleClick}>
+                                <Typography variant="h6" className={classes.avatar__text}>
+                                    Артур
+                                </Typography>
+                                <Avatar aria-label="recipe" color="primary" className={classes.avatar__image}>
+                                    {"Артур"[0].toUpperCase()}
+                                </Avatar>
+                                <ExpandMoreIcon className={classes.avatar__icon} />
+                            </Box>
                         </Toolbar>
                     </Grid>
                 </Grid>
--- components/Header/header.style.ts
+++ components/Header/header.style.ts
@@ -20,6 +20,49 @@ const HeaderStyles = makeStyles((theme: Theme) =>
         },
         toolbar: {
             minHeight: 48
+        },
+        avatar:{
+            display: "flex",
+            alignItems: "center",
+            cursor: "pointer",
+            height: 48,
+            padding: "0px 12px 0px 15px",
+            transition: "all .2s linear",
+            '&:hover': {
+                background: theme.palette.primary.dark
+            }
+        },
+        avatar__text:{
+            marginRight: 7,
+            fontSize: 17
+        },
+        avatar__image:{
+            height: 30,
+            width: 30
+        },
+        avatar__icon:{
+            marginLeft: 3,
+            fontSize: 18
+        },
+        menu:{
+            padding: 0,
+            marginTop: 35,
+        },
+        poppup:{
+            border: "1px solid #ccc",
+            background: "#fff",
+            padding: "10px 20px",
+
+        },
+        poppup__title:{
+            marginBottom: 10,
+            fontSize: 20
+        },
+        poppup__link:{
+            display: "block",
+            marginBottom: 5,
+            fontSize: 17,
+            padding: "10px 5px"
         }
     }),
 );
